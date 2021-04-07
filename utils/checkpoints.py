8e1431dc86e1654b0b0382306e9e3a7b42ab5fe1
PyLink88 <francesco.cannarile@eni.com>
2021-04-07 12:17:52 +0200

    utils added

---
+++ utils/checkpoints.py
@@ -0,0 +1,16 @@
+
+import os
+
+def checkpoints_folder(config):
+    
+    # Path of the folfer where to save checkpoint
+    path = config.checkpoint_dir + config.exp_name
+
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print('Create checkpoints folder: ' + path)
+    else:
+        print('Checkpoints folder ' + path + ' already exists')
+
+    return path + "/"
+
