1343318525c7753440073d66202d8e04bb3a5fb8
Francesco <53812589+PyLink88@users.noreply.github.com>
2021-05-19 09:56:48 +0200

    Update and rename ecg5000.py to common_loader.py

--- datasets/common_loader.py
+++ datasets/common_loader.py
@@ -1,7 +1,6 @@
 """
-ECG5000 Dataloader implementation, used in RNN_Autoencoder
+Common Dataloader implementation, used in RNN_Autoencoder
 """
-
 import numpy as np
 from utils.samplers import StratifiedSampler
 
@@ -9,8 +8,8 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset, Dataset
 
 
-class ECG500DataLoader:
-    def __init__(self, config):
+class RecAEDataLoader:
+    def __init__(self, batch_size, config):
         self.config = config
 
         # Loading training data
@@ -22,7 +21,7 @@ class ECG500DataLoader:
             # If loss with AUC penalty is used
             X_train = np.load(self.config.data_folder + self.config.X_train_p).astype(np.float32)
             y_train = np.load(self.config.data_folder + self.config.y_train_p).astype(np.float32)
-
+        
         # Loading validation data to control model training
         X_val = np.load(self.config.data_folder + self.config.X_val).astype(np.float32)
         y_val = np.load(self.config.data_folder + self.config.y_val).astype(np.float32)
@@ -44,16 +43,15 @@ class ECG500DataLoader:
 
         # Dataloader
         if self.config.training_type == 'one_class':
-
-            self.train_loader = DataLoader(training, batch_size=self.config.batch_size, shuffle=True)
+            self.train_loader = DataLoader(training, batch_size = batch_size, shuffle = True)
         else:
 
             sampler = StratifiedSampler(y_train,
-                                        batch_size=self.config.batch_size,
-                                        random_state=self.config.sampler_random_state)
-            self.train_loader = DataLoader(training, batch_sampler=sampler)
+                                        batch_size = batch_size,
+                                        random_state =self.config.sampler_random_state)
+            self.train_loader = DataLoader(training, batch_sampler = sampler)
 
-        self.valid_loader = DataLoader(validation, batch_size=self.config.batch_size_val, shuffle=False)
+        self.valid_loader = DataLoader(validation, batch_size = self.config.batch_size_val, shuffle = False)
 
         # Number of batches
         self.train_iterations = len(self.train_loader)
@@ -61,6 +59,3 @@ class ECG500DataLoader:
 
     def finalize(self):
         pass
-
-
-
